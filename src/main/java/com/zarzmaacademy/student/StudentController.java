070eb83d56293cea2a7b4e8486e12d0c67dbefb2
shako <davitashvilishako1@gmail.com>
2021-09-29 20:23:08 +0500

    added delete and update methods

--- src/main/java/com/zarzmaacademy/student/StudentController.java
+++ src/main/java/com/zarzmaacademy/student/StudentController.java
@@ -2,6 +2,7 @@ package com.zarzmaacademy.student;
 
 
 import com.zarzmaacademy.model.Student;
+import javassist.NotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,11 +18,25 @@ public class StudentController {
 
 	@GetMapping
 	public List<Student> getStudents() {
-		return studentRepository.findAll();
+		return studentRepository.findByActive(true);
 	}
 
 	@PostMapping
 	public Student addStudent(@RequestBody Student student) {
 		return studentRepository.save(student);
 	}
+
+	@PutMapping("{id}")
+	public Student updateStudent(@PathVariable Long id, @RequestBody Student student) {
+		student.setId(id);
+		return studentRepository.save(student);
+	}
+
+	@DeleteMapping("{id}")
+	public void deleteStudent(@PathVariable Long id) throws NotFoundException {
+		Student student = studentRepository.findById(id).orElseThrow(() -> new NotFoundException("student not found "));
+		student.setActive(false);
+		studentRepository.save(student);
+	}
+
 }
--- src/main/java/com/zarzmaacademy/student/StudentRepository.java
+++ src/main/java/com/zarzmaacademy/student/StudentRepository.java
@@ -2,8 +2,15 @@ package com.zarzmaacademy.student;
 
 import com.zarzmaacademy.model.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
+
+
+	List<Student> findByActive(Boolean active);
+
 }
